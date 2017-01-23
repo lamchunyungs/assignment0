7f1032cdf0fc88d509c59cf1dec6c08ff3341fc1
Calvin Kam <calvinkam123@gmail.com>
2017-01-24 04:25:38 +0800

    Assignment 0 Code Released

---
+++ debug/doMath.c
@@ -0,0 +1,9 @@
+/***************************** 
+* Program Code for doMath()
+*****************************/
+#include "doMath.h"
+double doMath(int a,int b) {
+	double c;
+	c = pow(a,b);
+	return c;
+}
---
+++ wordCount/count.c
@@ -0,0 +1,11 @@
+/***********************************
+* Program code for count()
+***********************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "util.h"
+
+void count(int argc,char *argv[])
+{
+	/* Write Your Code HERE! */
+}
